f3fee12f4d9421370bda8557717dba53d6016800
Andrzej Stencel <astencel@sumologic.com>
2021-11-18 18:16:44 +0100

    fix(enhance_k8s_metadata): remove API group unavailable in k8s 1.22
    The `extensions/v1beta1` API group has been unneeded for some time now,
    since all the resources supported by this plugin - DaemonSet, Deployment, ReplicaSet,
    StatefulSet - have been moved to `apps/v1` API group.
    The API group has ben ultimately removed in Kubernetes 1.22, causing errors
    when trying to create a client for it.

--- fluent-plugin-enhance-k8s-metadata/test/helper.rb
+++ fluent-plugin-enhance-k8s-metadata/test/helper.rb
@@ -24,28 +24,26 @@ def stub_apis
   stub_request(:any, %r{/apis$})
     .to_return(
       'body' => {
-        'versions' => ['apps/v1', 'extensions/v1beta1']
+        'versions' => ['apps/v1']
       }.to_json
     )
   stub_request(:get, %r{/api/v1$})
     .to_return(body: test_resource('api_list_core_v1.json'), status: 200)
   stub_request(:get, %r{/apis/apps$})
     .to_return(body: test_resource('api_list_apps.json'), status: 200)
-  stub_request(:get, %r{/apis/extensions$})
-    .to_return(body: test_resource('api_list_extensions.json'), status: 200)
   stub_request(:get, %r{/api/v1/endpoints$})
     .to_return(body: test_resource('endpoints_list.json'), status: 200)
   stub_request(:get, %r{/api/v1/namespaces/sumologic/pods})
     .to_return(body: test_resource('pod_sumologic.json'), status: 200)
-  stub_request(:get, %r{/apis/extensions/v1beta1/namespaces/sumologic/replicasets})
+  stub_request(:get, %r{/apis/apps/v1/namespaces/sumologic/replicasets})
     .to_return(body: test_resource('rs_sumologic.json'), status: 200)
-  stub_request(:get, %r{/apis/extensions/v1beta1/namespaces/sumologic/deployments})
+  stub_request(:get, %r{/apis/apps/v1/namespaces/sumologic/deployments})
     .to_return(body: test_resource('deploy_sumologic.json'), status: 200)
   stub_request(:get, %r{/api/v1/namespaces/kube-system/pods})
     .to_return(body: test_resource('pod_kube-system.json'), status: 200)
-  stub_request(:get, %r{/apis/extensions/v1beta1/namespaces/kube-system/replicasets})
+  stub_request(:get, %r{/apis/apps/v1/namespaces/kube-system/replicasets})
     .to_return(body: test_resource('rs_kube-system.json'), status: 200)
-  stub_request(:get, %r{/apis/extensions/v1beta1/namespaces/kube-system/deployments})
+  stub_request(:get, %r{/apis/apps/v1/namespaces/kube-system/deployments})
     .to_return(body: test_resource('deploy_kube-system.json'), status: 200)
   stub_request(:get, %r{/api/v1/namespaces/non-exist/pods})
     .to_raise(Kubeclient::ResourceNotFoundError.new(404, nil, nil))
@@ -55,7 +53,7 @@ end
 def stub_pod_with_nonexistent_owner
   stub_request(:get, %r{/api/v1/namespaces/sumologic/pods/pod-with-nonexistent-owner})
     .to_return(body: test_resource('pod_with_nonexistent_owner.json'), status: 200)
-  stub_request(:get, %r{/apis/extensions/v1beta1/namespaces/sumologic/replicasets/non-existent-replicaset})
+  stub_request(:get, %r{/apis/apps/v1/namespaces/sumologic/replicasets/non-existent-replicaset})
     .to_return(status: 404)
 end
 
@@ -70,5 +68,5 @@ def init_globals
   @cache_size = 1000
   @cache_ttl = 60 * 60
   @core_api_versions = ['v1']
-  @api_groups = ['apps/v1', 'extensions/v1beta1']
+  @api_groups = ['apps/v1']
 end
