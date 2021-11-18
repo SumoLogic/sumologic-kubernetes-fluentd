c43657b0a34ada3b926cfbe97e61fcc7cdf16ff2
Andrzej Stencel <astencel@sumologic.com>
2021-11-18 18:16:44 +0100

    fix(enhance_k8s_metadata): remove API group unavailable in k8s 1.22
    The `extensions/v1beta1` API group has been unneeded for some time now,
    since all the resources supported by this plugin - DaemonSet, Deployment, ReplicaSet,
    StatefulSet - have been moved to `apps/v1` API group.
    The API group has ben ultimately removed in Kubernetes 1.22, causing errors
    when trying to create a client for it.

--- fluent-plugin-enhance-k8s-metadata/test/sumologic/kubernetes/test_reader.rb
+++ fluent-plugin-enhance-k8s-metadata/test/sumologic/kubernetes/test_reader.rb
@@ -57,7 +57,7 @@ class ReaderTest < Test::Unit::TestCase
     assert_not_nil metadata
     assert_equal '1691804714', metadata['pod_labels']['pod_labels']['pod-template-hash']
     assert_empty metadata['owners']
-    expected_log_message = '[warn]: failed to fetch resource: replicasets, name: non-existent-replicaset, ns:sumologic with API version extensions/v1beta1'
+    expected_log_message = '[warn]: failed to fetch resource: replicasets, name: non-existent-replicaset, ns:sumologic with API version apps/v1'
     assert(
       log.logs.any? { |log| log.include?(expected_log_message) },
       "'#{expected_log_message}' not found in logs: #{log.logs}"
