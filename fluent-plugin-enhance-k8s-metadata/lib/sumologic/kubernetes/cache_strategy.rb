fcbd3d80a6412911deed0d41c0c48abb709d3b0c
gizmo-rt <pawan.murarka@acquia.com>
2021-12-13 19:09:49 +0530

    Updated cache refresh delay variable name

--- fluent-plugin-enhance-k8s-metadata/lib/sumologic/kubernetes/cache_strategy.rb
+++ fluent-plugin-enhance-k8s-metadata/lib/sumologic/kubernetes/cache_strategy.rb
@@ -29,7 +29,7 @@ module SumoLogic
             refresh_cache_entry(key)
             # Adding friction to avoid aggressive refresh.
             if !@cache_refresh_apiserver_request_delay.nan? && !@cache_refresh_apiserver_request_delay.negative?
-              sleep @cache_refresh_delay
+              sleep @cache_refresh_apiserver_request_delay
             end
           rescue => e
             log.error "Cannot refresh metadata for key #{key}: #{e}"
