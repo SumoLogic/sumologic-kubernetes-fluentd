6da81599c396ae637bd08398174e25ab1bc8df79
Pawan Raj Murarka <pawan.murarka@acquia.com>
2022-03-15 15:23:00 +0530

    Updated check and Added unit tests

--- fluent-plugin-enhance-k8s-metadata/lib/sumologic/kubernetes/cache_strategy.rb
+++ fluent-plugin-enhance-k8s-metadata/lib/sumologic/kubernetes/cache_strategy.rb
@@ -40,8 +40,9 @@ module SumoLogic
       def refresh_cache_entry(cache_key)
         log.debug "Refreshing metadata for key #{cache_key}"
         namespace_name, pod_name = cache_key.split("::")
-        if !@cache_refresh_exclude_pod_regex.empty? && Regexp.compile(@cache_refresh_exclude_pod_regex).match(pod_name)
+        if !@cache_refresh_exclude_pod_regex.to_s.strip.empty? && Regexp.compile(@cache_refresh_exclude_pod_regex).match(pod_name)
             @cache.delete(cache_key)
+            return nil
         end
         metadata = fetch_pod_metadata(namespace_name, pod_name)
         if metadata.empty?
