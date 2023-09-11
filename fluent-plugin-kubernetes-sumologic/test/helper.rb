717dda54714465112a2e209421fa210bd68e381c
Andrzej Stencel <astencel@sumologic.com>
2023-09-11 15:17:18 +0200

    chore: remove simplecov
    We don't use code coverage.

--- fluent-plugin-kubernetes-sumologic/test/helper.rb
+++ fluent-plugin-kubernetes-sumologic/test/helper.rb
@@ -1,11 +1,3 @@
-require "simplecov"
-SimpleCov.start
-
-if ENV["CI"] == "true"
-  require "codecov"
-  SimpleCov.formatter = SimpleCov::Formatter::Codecov
-end
-
 $LOAD_PATH.unshift(File.expand_path("../../", __FILE__))
 require "test-unit"
 require "fluent/test"
